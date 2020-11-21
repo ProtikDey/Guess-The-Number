366579b2c2dec9175f4df0231bb5494ed5dcc9cd
protikdey <protikdey490@gmail.com>
2020-11-21 20:24:34 +0600

    Checked wheather the input is valid or not

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -1,4 +1,20 @@
 'use strict';
 
 //display the textcontent of the 'between' class of the html
-console.log(document.querySelector('.between').textContent);
+//console.log(document.querySelector('.between').textContent);
+
+//taking the value of input box
+//document.querySelector('.guess').value;
+
+
+function clickEvent(){
+
+    const input = Number(document.querySelector('.guess').value) ;
+
+    if(!input){
+        document.querySelector('.message').textContent = 'Not a number!!!! Input a number';
+    }
+}
+
+
+document.querySelector('.check').addEventListener('click', clickEvent());

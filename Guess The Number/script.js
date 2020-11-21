2e1e502c2c729b3540cff78ea0246e28e34d4607
protikdey <protikdey490@gmail.com>
2020-11-21 21:38:45 +0600

    score logic done

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = randomNumber;
 
 
@@ -21,11 +22,33 @@ document.querySelector('.check').addEventListener('click', function(){
 
     //check wheather the input is too high
     else if(input > randomNumber){
-        document.querySelector('.message').textContent = 'Too high!!!';
+        //if score is greater than 1, then score--
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!!!';
+            score -- ;
+            document.querySelector('.score').textContent = score;
+
+        }
+        //if score is 0 then fix the score at 0 and game lost
+        else{
+            document.querySelector('.message').textContent = 'Game Lost!!!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 
     else {
-        document.querySelector('.message').textContent = 'Too low!!!';
+        //if score is greater than 1, then score--
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!!!';
+            score -- ;
+            document.querySelector('.score').textContent = score;
+    
+        }
+        //if score is 0 then fix the score at 0 and game lost
+        else{
+            document.querySelector('.message').textContent = 'Game Lost!!!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 
 });
